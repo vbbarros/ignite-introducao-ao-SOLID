e8df0dd415453dfaefb93374170a213e2c283c47
vbbarros <33907571+vbbarros@users.noreply.github.com>
2021-08-26 22:27:25 -0300

    Challenge finished

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,55 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const currentDate = new Date();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: currentDate,
+      updated_at: currentDate,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+
+    if (userIndex >= 0) {
+      const user = this.users[userIndex];
+
+      const currentDate = new Date();
+
+      user.admin = true;
+      user.updated_at = currentDate;
+
+      return user;
+    }
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+
+    const userRequesting = users.find((user) => user.id === user_id);
+
+    if (!userRequesting || !userRequesting.admin) {
+      throw new Error("Usuário não tem permissão para listar usuários");
+    }
+
+    return users;
   }
 }
 
